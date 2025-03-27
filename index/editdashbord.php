5c7045d3937289aab6c807b5a10640235188c3ff
chonlachit <chonlachit2233@gmail.com>
2025-03-27 22:13:31 +0700

    งานสมบูรณ์

--- index/editdashbord.php
+++ index/editdashbord.php
@@ -131,7 +131,9 @@ $activities = $query->fetchAll();
 
                         <!-- Earnings (Monthly) Card Example -->
                                                 <?php
-                        $sql = "SELECT ma_name FROM manageactivity"; 
+                        $sql = "SELECT activities, COUNT(*) AS user FROM  users ma LEFT JOIN manageactivity u ON ma_name = activities GROUP BY ma_name;
+                        ";
+
                         $query = $pdo->prepare($sql);
                         $query->execute();
                         $activities = $query->fetchAll();
@@ -148,9 +150,11 @@ $activities = $query->fetchAll();
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold <?= $texts[array_rand($texts)] ?> text-uppercase mb-1">
-                                                    <h6>ชื่อกิจกรรม: <?=$activity['ma_name']?></h6>
+                                                    <h6>ชื่อกิจกรรม: <?=$activity['activities']?></h6>
                                                 </div>
-                                                <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                    จำนวนผู้ลงทะเบียน: <?=$activity['user']?>
+                                                </div>
                                             </div>
                                             <div class="col-auto">
                                                 <i class="fas fa-calendar-alt fa-2x text-gray-300"></i>
@@ -159,8 +163,9 @@ $activities = $query->fetchAll();
                                     </div>
                                 </div>
                             </div>
-                        <?php } ?>
-
+                        <?php 
+                      } 
+                      ?>
 
                         <!-- Earnings (Monthly) Card Example -->
                        
@@ -294,8 +299,8 @@ $activities = $query->fetchAll();
       <!--end::App Main-->
       <!--begin::Footer-->
       <script src="../admin/js/Chart.min.js"></script>
-      <script src="../admin/js/chart-area2-demo.js"></script>
-      <script src="../admin/js/chart-pie2-demo.js"></script>
+      <script src="../admin/js/chart-area3-demo.js"></script>
+      <script src="../admin/js/chart-pie3-demo.js"></script>
 
    
     </div>
